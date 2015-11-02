b24e4e506058c496c80395cb53102de968f96b50
onyoo <roberto9m9@gmail.com>
2015-11-03 00:10:44 +0100

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,18 @@
 # Implement your procedural solution here!
+
+def largest_palindrome_product
+  high = 999
+  high_set = (high-100..high).to_a
+
+  #find target products
+  perm = high_set.permutation(2).to_a
+  perm_prod = perm.map {|set| set[0]*set[1]}.uniq
+
+  perm_prod = perm_prod.reverse
+
+  #slices and matches 
+  x = perm_prod.select do |prod|
+    prod.to_s.byteslice(0,3) == prod.to_s.byteslice(3,5).reverse
+    end
+  encode_answer = x.max
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,23 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+
+  def largest_palindrome_product
+#requirement to pass test.
+  end
+
+  def answer
+    #find target products
+    @high = 999
+    @high_set = (@high - 100..@high).to_a
+    @perm = @high_set.permutation(2).to_a
+    @perm_prod = @perm.map {|set| set[0]*set[1]}.uniq
+
+    @perm_prod = @perm_prod.reverse
+
+    #slices and matches 
+    @x = @perm_prod.select do |prod|
+      prod.to_s.byteslice(0,3) == prod.to_s.byteslice(3,5).reverse
+      end
+    @encode_answer = @x.max
+  end
+end
